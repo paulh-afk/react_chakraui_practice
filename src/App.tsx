08cb8d42ffa1c5547d3f258439915216546a620f
Paul <paul.helleu@orange.fr>
2022-09-17 16:25:33 +0200

    chakra-ui init

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import * as React from 'react';
+import { Box } from '@chakra-ui/react';
+
 export const App = () => {
   return (
-    <div>
-      <h1>Hello, world!</h1>
-    </div>
+    <Box bg={'tomato'} p={4} color={'white'}>
+      This is a Box
+    </Box>
   );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ChakraProvider } from '@chakra-ui/react';
 import { App } from './App';
 
 const container = document.getElementById('root');
 if (!container) throw new Error('Can find root element!');
 const root = ReactDOM.createRoot(container);
 
-root.render(<App />);
+root.render(
+  <ChakraProvider>
+    <App />
+  </ChakraProvider>,
+);
