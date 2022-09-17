2927700d889334dc91197ecdb53089b0901ac6bd
Paul <paul.helleu@orange.fr>
2022-09-17 17:25:48 +0200

    Box, Center, Icons components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
 import * as React from 'react';
-import { Box } from '@chakra-ui/react';
+import { Box, Center, HStack, Circle } from '@chakra-ui/react';
+import { PhoneIcon } from '@chakra-ui/icons';
 
 export const App = () => {
   return (
-    <Box bg={'tomato'} p={4} color={'white'}>
-      This is a Box
-    </Box>
+    <>
+      <Box bg={'tomato'} p={4} color={'white'}>
+        This is a Box
+      </Box>
+      <Center bg="blueviolet" h="100px" color="white">
+        This is the Center
+      </Center>
+      <HStack>
+        <Circle bg={'green.500'} p={4}>
+          <PhoneIcon color={'white'} />
+        </Circle>
+      </HStack>
+    </>
   );
 };
